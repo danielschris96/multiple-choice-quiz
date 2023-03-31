c7d24d89a056b16cf5458e8c08c215189d9bf73c
Chris Daniels <danielschris96@gmail.com>
2023-03-30 22:25:23 -0700

    added 10 multiple choice questions to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+/* Fundamentals questions:
+
+What method would be used to output a random number between 0 and 1?
+Math.random()
+Math.floor()
+Math.log()
+Array.splice()
+
+What method would be used to add an element to the beginning of an array?
+push()
+splice()
+unshift()
+includes()
+
+What method would be used to extract a part of a string and return a new string?
+split()
+slice()
+replace()
+toString()
+
+In JavaScript, an entity that holds keys and values is called a(n)
+Array
+Object
+String
+Function
+
+Which of the following is not a type of loop in JavaScript?
+for loop
+while loop
+do...while loop
+when...do loop
+
+Which of the following is the proper notation for changing text color through JavaScript?
+divTag.setAttribute("color":"blue")
+divTag.setAttribute("style", "color: blue")
+divTag.setAttribute("color: blue")
+divTag.setAttribute("style"; "color: blue")
+
+Which of the following is not an event that can be paired with an eventListener()?
+mouseover
+keydown
+click
+hover
+
+Which logical operator denotes 'or'?
+||
+&&
+!
+===
+
+In the following array, which index postion equates to 'Apple'?
+array = ["Apple", "Banana", "Pear", "Orange"]
+[-1]
+[0]
+[1]
+None of the above
+
+A function within an object is called a(n)
+Task
+Duty
+Role
+Method
+*/

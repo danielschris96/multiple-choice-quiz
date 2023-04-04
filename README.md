26494fffcc0cf6a4279081fb51e9403b4c0c11c8
Chris Daniels <danielschris96@gmail.com>
2023-04-03 22:04:53 -0700

    finishing touches on styling an js function. completed readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# multiple-choice-quiz
-Multiple choice quiz on JavaScript fundamentals
+# JavaScript Fundamentals Quiz
+
+## Description
+
+This JavaScript fundamentals quiz is a multiple choice quiz that once completed, saves a score for how quickly the quiz was completed. The score, along with the user's intials, will be saved and shown on the leaderboard. 
+
+The purpose of this exercise was to deploy newly aquired JavaScript techniques to write a dynamic application that goes beyond the scope of a static webpage. This page was built from scratch using HTML, CSS, and JavaScript.
+
+This application is deployed on GitHub pages:
+<br> https://danielschris96.github.io/multiple-choice-quiz/
+
+## Usage
+
+Once the page is loaded, the quiz can be started by clicking the "Start" button. The user selects an answer and is either docked 10 seconds for an incorrect answer, or moves to the next question. There are a total of 8 questions in this quiz.
+
+Once complete, the user enters their initials and their score will be saved to local storage and the leaderboard. The user can reset the leaderboard with the "Reset scores" button.
+
+![Screenshot of webpage](./assets/images/page-screenshot.JPG)
+
+## Credits
+
+https://www.w3schools.com/jsref/prop_win_localstorage.asp<br>
+https://www.w3schools.com/jsref/met_document_createelement.asp<br>
+https://www.w3schools.com/jsref/prop_style_visibility.asp<br>
+https://www.w3schools.com/jsref/met_document_addeventlistener.asp<br>
+https://www.w3schools.com/jsref/met_document_queryselector.asp
+
+## License
+
+None
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-
+// defining all global variables. set questions and answers into arrays to select upon later
 var question1 = ["What method would be used to output a random number between 0 and 1?", "Math.random()", "Math.floor()", "Math.log()", "Array.splice()"];
 var question2 = ["What method would be used to add an element to the beginning of an array?", "push()", "splice()", "unshift()", "includes()"];
 var question3 = ["What method would be used to extract a part of a string and return a new string?", "split()", "slice()", "replace()", "toString()"];
 var question4 = ["In JavaScript, an entity that holds keys and values is called a(n)", "Array", "Object", "String", "Function"];
 var question5 = ["Which of the following is not a type of loop in JavaScript?", "for loop", "while loop", "do...while loop", "when...do loop"];
 var question6 = ["Which of the following is not an event that can be paired with an eventListener()?", "mouseover", "keydown", "click", "hover"];
-var question7 = ["Which logical operator denotes 'or'?", "||", "&&", "!", "==="];
+var question7 = ["Which logical operator denotes 'or'?", "||", "&&", "!==", "==="];
 var question8 = ["A function within an object is called a", "Task", "Duty", "Role", "Method"];
+// put all question+answer arrays into single array to iterate through arrays
 var totalList = [question1, question2, question3, question4, question5, question6, question7, question8];
 var timer = document.querySelector("#timer");
 var secondsRemaining = timer.getAttribute("data-seconds");
@@ -27,15 +28,17 @@ var resestBtn = document.querySelector('#reset-scores');
 var initialsColumn = document.querySelector('#initials-column');
 var scoresColumn = document.querySelector('#scores-column');
 var answerCounter = 0;
+// set array equal to answer positions based on question order
 var correctAnswers = ["#answer1", "#answer3", "#answer2", "#answer2", "#answer4", "#answer4", "#answer1", "#answer4"];
 var score = 0;
 
+// hidden elements when they are not needed
 container.style.visibility = "hidden";
 timerVisible.style.visibility = "hidden";
 saveScore.style.display = "none";
 leaderboard.style.display = "none";
 
-
+// function begins quiz, shows related elements
 document.addEventListener("click", function(event) {
     var element = event.target
     if (element.matches("#start")) {
@@ -47,6 +50,7 @@ document.addEventListener("click", function(event) {
     }
 })
 
+// starts timer, takes 10 seconds off for wrong answer. clears timer when last question is answered
 function startTimer() {
     secondsLeft = parseInt(timer.getAttribute("data-seconds"));
     var timeInterval = setInterval(function() {
@@ -62,6 +66,7 @@ function startTimer() {
 }, 1000)
 }
 
+// inputs next question/answers. once final question is answered, youWin fuction is called
 function nextQuestion() {
         
     if (answerCounter < 8) {
@@ -76,6 +81,7 @@ function nextQuestion() {
     }
 }
 
+// inputs initial question, listens for correct answer to increase answer counter
 function inputQuestion() {
     questionInput.textContent = question1[0];
     answer1.textContent = question1[1];
@@ -94,6 +100,7 @@ function inputQuestion() {
     })
 }
 
+// hides elements and prompts user to refresh to try again
 function youLost() {
     questionInput.textContent = "You lost. Refresh page to try again.";
     answer1.style.display = "none";
@@ -103,6 +110,7 @@ function youLost() {
     timerVisible.style.display = "none";
 }
 
+// pulls a final score and hides elements
 function youWin() {
     var finalScore = secondsLeft 
     container.style.display = "none";
@@ -112,8 +120,10 @@ function youWin() {
     
 }
 
+// stores initals and scores into local storage. pulls all local storage and puts keys/values into respective list items. appends list items to leaderboard lists.
 function scoreSubmit() {
     saveScore.style.display = "none";
+    // secondsLeft + 1 equals actual score because of slight delay for clearing the timer
     localStorage.setItem(initials.value, secondsLeft +1);
     leaderboard.style.display = "block";
   for (var i = 0; i < localStorage.length; i++) {
@@ -129,76 +139,15 @@ function scoreSubmit() {
 
 }
 
-
+// listens for the submit button, triggers scoreSubmit function
 submitBtn.addEventListener("click", function(event) {
     event.preventDefault();
     scoreSubmit();
 })
 
+// listens for Reset Scores button, clears local storage and leaderboard
 resestBtn.addEventListener("click", function(event) {
     localStorage.clear();
+    scoresColumn.style.display = "none";
+    initialsColumn.style.display = "none";
 })
-
-/* Fundamentals questions:
-
-What method would be used to output a random number between 0 and 1?
-Math.random()
-Math.floor()
-Math.log()
-Array.splice()
-
-What method would be used to add an element to the beginning of an array?
-push()
-splice()
-unshift()
-includes()
-
-What method would be used to extract a part of a string and return a new string?
-split()
-slice()
-replace()
-toString()
-
-In JavaScript, an entity that holds keys and values is called a(n)
-Array
-Object
-String
-Function
-
-Which of the following is not a type of loop in JavaScript?
-for loop
-while loop
-do...while loop
-when...do loop
-
-Which of the following is the proper notation for changing text color through JavaScript?
-divTag.setAttribute("color":"blue")
-divTag.setAttribute("style", "color: blue")
-divTag.setAttribute("color: blue")
-divTag.setAttribute("style"; "color: blue")
-
-Which of the following is not an event that can be paired with an eventListener()?
-mouseover
-keydown
-click
-hover
-
-Which logical operator denotes 'or'?
-||
-&&
-!
-===
-
-In the following array, which index postion equates to 'Apple'?
-array = ["Apple", "Banana", "Pear", "Orange"]
-[-1]
-[0]
-[1]
-None of the above
-
-A function within an object is called a
-Task
-Duty
-Role
-Method
-*/
